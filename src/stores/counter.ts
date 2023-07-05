76fe8841a7a0cfcd8bb8d3cc5573c4ab8d028d62
Ouzinem <ouzinem@hotmail.com>
2023-07-05 14:42:06 -0400

    using ref and nextTick(async exemple)

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,8 +1,9 @@
-import { computed, ref } from 'vue'
+import { computed, nextTick, ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+  const title = ref('Counter')
   const doubleCount = computed(() => count.value * 2)
   const isOddOrEven = computed(() => (count.value % 2 === 0 ? 'even' : 'odd'))
 
@@ -14,14 +15,33 @@ export const useCounterStore = defineStore('counter', () => {
     count.value++
   }
 
-  function incrementCountPlus2(amount: number, e: MouseEvent) {
+  // function incrementCountPlus2(amount: number, e: MouseEvent) {
+  //   // console.log(e)
+  //   count.value += amount
+  //   nextTick(() => {
+  //     console.log('count', count.value)
+  //   })
+  // }
+  // change for async function
+  async function incrementCountPlus2Async(amount: number, e: MouseEvent) {
     // console.log(e)
     count.value += amount
+    await nextTick()
+    console.log('count', count.value)
   }
+
   function decrementCount() {
     if (count.value === 0) return
     count.value--
   }
 
-  return { count, decrementCount, incrementCount, incrementCountPlus2, doubleCount, isOddOrEven }
+  return {
+    title,
+    count,
+    decrementCount,
+    incrementCount,
+    incrementCountPlus2Async,
+    doubleCount,
+    isOddOrEven
+  }
 })
